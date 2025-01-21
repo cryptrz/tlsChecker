62f3d1f86ccbb9093edf84e5f99b15c41d3d9f27
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-01-21 14:43:38 +0100

    Update tlschecker.sh
    Increased from 2 weeks to 1 month

--- tlschecker.sh
+++ tlschecker.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Check root
 if [[ $(id -u) -ne 0  ]];then
         echo "Please execute as root"
         exit 1
 fi
 
-# Check if 1 argument is added
 if [[ $# -ne 1 ]]; then
         echo "Usage: $0 example.com"
         exit 1
 fi
 
-# Create variables
 validity=$(nmap --script ssl-cert $1 | grep "Not valid after" | cut -d ":" -f 2 | cut -d "T" -f 1)
-twoweeks=$(date -d "+2 weeks" +"%Y-%m-%d")
+twoweeks=$(date -d "+1 month" +"%Y-%m-%d")
 
-#Check the validity
 if [[ "$validity" < "$twoweeks" ]]; then
     echo "Update NOW!"
 else
