4cf68e3d27313add09859d45a02e9f34fd4e6293
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-01-21 14:48:06 +0100

    Update tlschecker.sh
    Renamed $twoweeks variable to $onemonth and added comments

--- tlschecker.sh
+++ tlschecker.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Check root user
 if [[ $(id -u) -ne 0  ]];then
         echo "Please execute as root"
         exit 1
 fi
 
+# Check if 1 argument is added
 if [[ $# -ne 1 ]]; then
         echo "Usage: $0 example.com"
         exit 1
 fi
 
+# Create variables
 validity=$(nmap --script ssl-cert $1 | grep "Not valid after" | cut -d ":" -f 2 | cut -d "T" -f 1)
-twoweeks=$(date -d "+1 month" +"%Y-%m-%d")
+onemonth=$(date -d "+1 month" +"%Y-%m-%d")
 
-if [[ "$validity" < "$twoweeks" ]]; then
+# Check validity
+if [[ "$validity" < "$onemonth" ]]; then
     echo "Update NOW!"
 else
         echo "Everything is fine, the certificate is valid until:$validity"
